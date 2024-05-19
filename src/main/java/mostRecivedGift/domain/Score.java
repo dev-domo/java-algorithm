e2e27f7d4dc780d812849908076e2bbdc99da5e9
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-19 16:19:18 +0900

    FEAT : 선물지수 증감 메소드와 서로 다른 친구의 선물지수를 비교하는 메소드 추가

--- src/main/java/mostRecivedGift/domain/Score.java
+++ src/main/java/mostRecivedGift/domain/Score.java
@@ -1,2 +1,45 @@
-package mostRecivedGift.domain;public class Score {
+package mostRecivedGift.domain;
+
+import java.util.Arrays;
+
+public class Score {
+    private int[] giftScore;
+
+    public Score(int[] giftScore) {
+        this.giftScore = giftScore;
+    }
+
+    public Score(int capacity) {
+        this.giftScore = makeScore(capacity);
+    }
+
+    private int[] makeScore(int capacity) {
+        this.giftScore = new int[capacity];
+        return this.giftScore;
+    }
+
+    public void add(int index) {
+        this.giftScore[index]++;
+    }
+
+    public void minus(int index) {
+        this.giftScore[index]--;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof Score)) return false;
+        Score score = (Score) object;
+        return Arrays.equals(giftScore, score.giftScore);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(giftScore);
+    }
+
+    public boolean biggerScore(int index1, int index2) {
+        return this.giftScore[index1] > this.giftScore[index2];
+    }
 }

aa703ca0b25060eb6ab3a2796e44d86afb2d8d0d
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-19 16:18:23 +0900

    FEAT : 선물 주고 받은 기록을 추가하고 가장 많이 받은 선물 개수를 구하는 메소드 추가

--- src/main/java/mostRecivedGift/domain/Record.java
+++ src/main/java/mostRecivedGift/domain/Record.java
@@ -1,28 +1,48 @@
 package mostRecivedGift.domain;
 
+import java.util.Arrays;
 import java.util.Objects;
 
-public class GiveAndTake {
-    private Friends friends;
-    public GiveAndTake(Friends friends) {
-        this.friends = friends;
+public class Record {
+    private int[][] giveTakeRecord;
+
+    public Record(int[][] example) {
+        this.giveTakeRecord = example;
+    }
+
+    public Record(int capacity) {
+        this.giveTakeRecord = new int[capacity][capacity];
+    }
+
+    public void writeRecord(int index1, int index2) {
+        this.giveTakeRecord[index1][index2]++;
+    }
+
+    public int findMax(Score score) {
+        int max = 0;
+        for (int i = 0; i < this.giveTakeRecord.length; i++) {
+            int cnt = 0;
+            for (int j = 0; j < this.giveTakeRecord[0].length; j++) {
+                if (this.giveTakeRecord[i][j] > this.giveTakeRecord[j][i])
+                    cnt++;
+                if (this.giveTakeRecord[i][j] == this.giveTakeRecord[j][i] && score.biggerScore(i, j))
+                    cnt++;
+            }
+            max = Math.max(max, cnt);
+        }
+        return max;
     }
 
     @Override
     public boolean equals(Object object) {
         if (this == object) return true;
-        if (!(object instanceof GiveAndTake)) return false;
-        GiveAndTake that = (GiveAndTake) object;
-        return Objects.equals(friends, that.friends);
+        if (!(object instanceof Record)) return false;
+        Record record = (Record) object;
+        return Arrays.deepEquals(giveTakeRecord, record.giveTakeRecord);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(friends);
-    }
-
-    public int[][] makeRecord(Friends friends) {
-        int capacity = friends.calculateFriendNumber();
-        return new int[capacity][capacity];
+        return Arrays.deepHashCode(giveTakeRecord);
     }
 }

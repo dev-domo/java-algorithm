d617d8f2fd6b0a1f342f25596ff042e53c5af99d
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-27 15:45:25 +0900

    FEAT : Position 생성 - 현재 산책 위치 저장

---
+++ src/main/java/walkingPark/domain/Position.java
@@ -0,0 +1,36 @@
+package walkingPark.domain;
+
+import java.util.Objects;
+
+public class Position {
+    private int row;
+    private int col;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Position))
+            return false;
+        Position position = (Position) o;
+        return row == position.row && col == position.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
+
+    public void moveRow(int move) {
+        this.row += move;
+    }
+
+    public void moveCol(int move) {
+        this.col += move;
+    }
+}
---
+++ src/test/java/walkingPark/domain/PositionTest.java
@@ -0,0 +1,2 @@
+package walkingPark;public class PositionTest {
+}

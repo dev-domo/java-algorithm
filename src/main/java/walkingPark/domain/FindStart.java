d5d8b0526421e387693dec32d394b8daa7fe00c5
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-27 15:46:01 +0900

    FEAT : FindStart 생성 - 산책 시작점 위치 찾음

--- src/main/java/walkingPark/domain/FindStart.java
+++ src/main/java/walkingPark/domain/FindStart.java
@@ -1,2 +1,16 @@
-package walkingPark.domain;public class FindStart {
+package walkingPark.domain;
+
+public class FindStart {
+    private static final String START = "S";
+
+    public static Position whereIsStart(String[] park) {
+        Position position = null;
+        for (int i = 0; i < park.length; i++) {
+            int idx = park[i].indexOf(START);
+            if (idx > 0) {
+                position = new Position(i, idx);
+            }
+        }
+        return position;
+    }
 }

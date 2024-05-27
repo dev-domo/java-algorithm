3c0d1b09e8103de98b4564599c22012dc0b1ff35
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-27 15:47:48 +0900

    FEAT : ResultView 생성 - 최종 산책 위치 출력

--- src/main/java/walkingPark/view/ResultView.java
+++ src/main/java/walkingPark/view/ResultView.java
@@ -1,4 +1,8 @@
-package walkingPark;
+package walkingPark.view;
 
 public class ResultView {
+    public static void printWalkingResult(int[] answer) {
+        System.out.println("산책을 종료합니다.");
+        System.out.println("현재 위치는 " + answer[0] + ", " + answer[1] + "입니다.");
+    }
 }

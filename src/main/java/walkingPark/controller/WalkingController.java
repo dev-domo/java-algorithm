9dcab81527c6f3a9a3fda81fe881d912d6bac2f8
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-29 15:46:30 +0900

    FEAT : 산책 controller 생성

--- src/main/java/walkingPark/controller/WalkingController.java
+++ src/main/java/walkingPark/controller/WalkingController.java
@@ -1,9 +1,19 @@
-package walkingPark;
+package walkingPark.controller;
 
-import mostRecivedGift.view.InputView;
+import walkingPark.domain.FindStart;
+import walkingPark.domain.Position;
+import walkingPark.domain.Walk;
+import walkingPark.view.InputView;
+import walkingPark.view.ResultView;
 
 public class WalkingController {
-    public static void main(String[] args) {
-        InputView.inputParkWidth();
+    public static void control() {
+        String[] park = InputView.inputParkArea(InputView.inputParkWidth(), InputView.inputParkHeight());
+        String[] routes = InputView.inputRoutes();
+
+        Position start = FindStart.whereIsStart(park);
+        Position end = Walk.startWalking(park, routes, start);
+
+        ResultView.printWalkingResult(end);
     }
 }

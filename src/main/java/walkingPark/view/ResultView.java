72cfddfe09f7d2c0007e5b82a3b8b84f339c01fc
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-29 15:49:25 +0900

    REFACTOR : Position에게 메시지를 보내 종료 지점을 구하도록 변경

--- src/main/java/walkingPark/view/ResultView.java
+++ src/main/java/walkingPark/view/ResultView.java
@@ -1,8 +1,10 @@
 package walkingPark.view;
 
+import walkingPark.domain.Position;
+
 public class ResultView {
-    public static void printWalkingResult(int[] answer) {
+    public static void printWalkingResult(Position position) {
         System.out.println("산책을 종료합니다.");
-        System.out.println("현재 위치는 " + answer[0] + ", " + answer[1] + "입니다.");
+        System.out.println("현재 위치는 " + position.getCurrent()[0] + ", " + position.getCurrent()[1] + "입니다.");
     }
 }

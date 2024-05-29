a31576518fdb6a525ab0136c1afa1a61a34db47b
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-29 15:45:55 +0900

    FEAT : main 생성

---
+++ src/main/java/walkingPark/WalkingMain.java
@@ -0,0 +1,9 @@
+package walkingPark;
+
+import walkingPark.controller.WalkingController;
+
+public class WalkingMain {
+    public static void main(String[] args) {
+        WalkingController.control();
+    }
+}
---
+++ src/main/java/walkingPark/controller/WalkingController.java
@@ -0,0 +1,9 @@
+package walkingPark;
+
+import mostRecivedGift.view.InputView;
+
+public class WalkingController {
+    public static void main(String[] args) {
+        InputView.inputParkWidth();
+    }
+}

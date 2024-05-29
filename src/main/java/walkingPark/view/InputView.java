d539f492718b94be02448a1e74180454d36d60ae
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-29 15:48:59 +0900

    REFACTOR : stringbuilder 제거. inputParkLine 제거. 입력 오류 해결을 위한 scan.nextLine() 추가

--- src/main/java/walkingPark/view/InputView.java
+++ src/main/java/walkingPark/view/InputView.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scan = new Scanner(System.in);
+    private static final int MAX_ROUTES = 3;
+
     public static int inputParkWidth() {
         System.out.print("공원의 가로 길이를 설정해주세요 : ");
         return scan.nextInt();
@@ -15,18 +17,21 @@ public class InputView {
     }
 
     public static String[] inputParkArea(int width, int height) {
+        System.out.println("공원을 조성해주세요 : ");
         String[] park = new String[height];
         for (int i = 0; i < height; i++) {
-            park[i] = inputParkLine(width);
+            park[i] = scan.next();
         }
+        scan.nextLine();
         return park;
     }
 
-    private static String inputParkLine(int width) {
-        StringBuilder str = new StringBuilder();
-        for (int j = 0; j < width; j++) {
-            str.append(scan.next());
+    public static String[] inputRoutes() {
+        System.out.println("루트를 설정해주세요 : ");
+        String[] routes = new String[MAX_ROUTES];
+        for (int i = 0; i < routes.length; i++) {
+            routes[i] = scan.nextLine();
         }
-        return str.toString();
+        return routes;
     }
 }

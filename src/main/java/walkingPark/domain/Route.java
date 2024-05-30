c6001f3acb100a6d08f5e43d03bb34470376dbea
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-30 10:05:51 +0900

    FEAT : Route 생성 - 원하는 이동 방향과 칸 수를 포함하는 객체

--- src/main/java/walkingPark/domain/Route.java
+++ src/main/java/walkingPark/domain/Route.java
@@ -1,2 +1,32 @@
-package walkingPark.domain;public class Route {
+package walkingPark.domain;
+
+import java.util.Objects;
+
+public class Route {
+    private String direct;
+    private int move;
+
+    public Route(String input) {
+        this(input.split(" ")[0], Integer.parseInt(input.split(" ")[1]));
+    }
+
+    public Route(String direct, int move) {
+        this.direct = direct;
+        this.move = move;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Route))
+            return false;
+        Route route = (Route) o;
+        return move == route.move && Objects.equals(direct, route.direct);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(direct, move);
+    }
 }

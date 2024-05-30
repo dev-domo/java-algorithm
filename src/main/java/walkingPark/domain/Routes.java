8cc313b9de65045496b7a0132a81fd9677763347
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-30 10:06:09 +0900

    FEAT : Routes 생성 - 여러 개의 Route를 포함하는 객체

--- src/main/java/walkingPark/domain/Routes.java
+++ src/main/java/walkingPark/domain/Routes.java
@@ -1,2 +1,25 @@
-package walkingPark.domain;public class Routes {
+package walkingPark.domain;
+
+import java.util.Arrays;
+
+public class Routes {
+    private Route[] route;
+    public Routes(Route[] route) {
+        this.route = route;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Routes))
+            return false;
+        Routes routes = (Routes) o;
+        return Arrays.equals(route, routes.route);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(route);
+    }
 }

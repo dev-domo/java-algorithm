facae6a99a05e46124874fe2fdc7fe96332bb691
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-30 10:05:09 +0900

    FEAT : Col 생성 - Position의 열을 표현하는 객체

--- src/main/java/walkingPark/domain/Col.java
+++ src/main/java/walkingPark/domain/Col.java
@@ -1,2 +1,30 @@
-package walkingPark.domain;public class Col {
+package walkingPark.domain;
+
+import java.util.Objects;
+
+public class Col {
+    private int num;
+    public Col(int num) {
+        this.num = num;
+    }
+
+    public Col update(int move) {
+        this.num += move;
+        return new Col(this.num);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Col))
+            return false;
+        Col col = (Col) o;
+        return num == col.num;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(num);
+    }
 }

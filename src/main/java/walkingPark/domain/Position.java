26133eb940cfe7e88523f7820bb92ddb4ad20e5f
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-30 10:04:45 +0900

    FEAT : Position 생성 - 공원 산책 위치 표현하는 객체

--- src/main/java/walkingPark/domain/Position.java
+++ src/main/java/walkingPark/domain/Position.java
@@ -1,2 +1,32 @@
-package walkingPark.domain;public class Position {
+package walkingPark.domain;
+
+import java.util.Objects;
+
+public class Position {
+    private Row row;
+    private Col col;
+
+    public Position(int row, int col) {
+        this(new Row(row), new Col(col));
+    }
+
+    public Position(Row row, Col col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Position))
+            return false;
+        Position position = (Position) o;
+        return Objects.equals(row, position.row) && Objects.equals(col, position.col);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
 }

6d60a84f2f0f9d61caac92e2b3e821408e8e83d2
gjtmdwns <gjtmdwns0815@naver.com>
2024-05-30 10:05:22 +0900

    FEAT : Row 생성 - Position의 행을 표현하는 객체

--- src/main/java/walkingPark/domain/Row.java
+++ src/main/java/walkingPark/domain/Row.java
@@ -1,2 +1,31 @@
-package walkingPark.domain;public class Row {
+package walkingPark.domain;
+
+import java.util.Objects;
+
+public class Row {
+    private int num;
+
+    public Row(int num) {
+        this.num = num;
+    }
+
+    public Row update(int move) {
+        this.num += move;
+        return new Row(this.num);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Row))
+            return false;
+        Row row = (Row) o;
+        return num == row.num;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(num);
+    }
 }

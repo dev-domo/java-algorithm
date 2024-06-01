6f9f24f64cd981f07794b8289c8c97f82661c844
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-02 00:20:02 +0900

    CHORE : 오탈자 수정

--- src/main/java/walkingPark/domain/Route.java
+++ src/main/java/walkingPark/domain/Route.java
@@ -3,6 +3,10 @@ package walkingPark.domain;
 import java.util.Objects;
 
 public class Route {
+    private static final String EAST = "E";
+    private static final String WEST = "W";
+    private static final String SOUTH = "S";
+    private static final String NORTH = "N";
     private String direct;
     private int move;
 
@@ -13,6 +17,25 @@ public class Route {
     public Route(String direct, int move) {
         this.direct = direct;
         this.move = move;
+    }
+
+    public Position tryMove(Park park, Row row, Col col) {
+        Row tmpRow = row;
+        Col tmpCol = col;
+        for (int i = 0; i < this.move; i++) {
+            if (this.direct.equals(EAST)) tmpCol = tmpCol.update(1);
+            if (this.direct.equals(WEST)) tmpCol = tmpCol.update(-1);
+            if (this.direct.equals(SOUTH)) tmpRow = tmpRow.update(1);
+            if (this.direct.equals(NORTH)) tmpRow = tmpRow.update(-1);
+            // Work here : 범위 벗어난 경우, 장애물 벗어난 경우
+            if (!tmpRow.isInRange(park.getHeight()) || !tmpCol.isInRange(park.getWidth()) || park.isMeetObstruction(tmpRow, tmpCol)) {
+                tmpRow = row;
+                tmpCol = col;
+                System.out.println("ho!");
+                break;
+            }
+        }
+        return new Position(tmpRow, tmpCol);
     }
 
     @Override

f3b577b3d374d36cf99481a9c92389973e3c0d99
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-02 14:48:31 +0900

    FEAT : WalkController 생성 - view와 Walk를 호출하는 컨트롤러

--- src/main/java/walkingPark/controller/WalkController.java
+++ src/main/java/walkingPark/controller/WalkController.java
@@ -1,2 +1,14 @@
-package walkingPark.controller;public class WalkController {
+package walkingPark.controller;
+
+import walkingPark.domain.Park;
+import walkingPark.domain.Routes;
+import walkingPark.domain.Walk;
+import walkingPark.view.InputView;
+import walkingPark.view.ResultView;
+
+public class WalkController {
+    public static void controlWalk() {
+        Walk.startWalk(InputView.inputParkArea(InputView.inputParkHeight()), InputView.inputRoutes());
+        ResultView.printWalkingResult();
+    }
 }

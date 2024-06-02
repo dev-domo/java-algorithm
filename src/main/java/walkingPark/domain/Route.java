db083e39561ec97fd58a0350ab4e68691c234765
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-02 16:13:33 +0900

    REFACTOR : tryMove()에서 방향에 따른 Row, Col 업데이트 시 Row와 Col에게 각각 메세지를 보내도록 수정

--- src/main/java/walkingPark/domain/Route.java
+++ src/main/java/walkingPark/domain/Route.java
@@ -3,12 +3,8 @@ package walkingPark.domain;
 import java.util.Objects;
 
 public class Route {
-    private static final String EAST = "E";
-    private static final String WEST = "W";
-    private static final String SOUTH = "S";
-    private static final String NORTH = "N";
-    private String direct;
-    private int move;
+    private final String direct;
+    private final int move;
 
     public Route(String input) {
         this(input.split(" ")[0], Integer.parseInt(input.split(" ")[1]));
@@ -24,10 +20,8 @@ public class Route {
         Col beforeCol = new Col(col);
 
         for (int i = 0; i < this.move; i++) {
-            if (this.direct.equals(EAST)) col = col.update(1);
-            if (this.direct.equals(WEST)) col = col.update(-1);
-            if (this.direct.equals(SOUTH)) row = row.update(1);
-            if (this.direct.equals(NORTH)) row = row.update(-1);
+            col = col.getNewCol(this.direct);
+            row = row.getNewRow(this.direct);
             if (!row.isInRange(park.getHeight()) || !col.isInRange(park.getWidth()) || park.isMeetObstruction(row, col)) {
                 row = beforeRow;
                 col = beforeCol;

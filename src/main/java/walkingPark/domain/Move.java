9302d8691cfa84e014d20e7b0fe6cb09979b4917
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-02 16:36:04 +0900

    FEAT : Move 객체 생성

--- src/main/java/walkingPark/domain/Move.java
+++ src/main/java/walkingPark/domain/Move.java
@@ -1,2 +1,29 @@
-package walkingPark.domain;public class Move {
+package walkingPark.domain;
+
+import java.util.Objects;
+
+public class Move {
+    private final int num;
+    public Move(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Move))
+            return false;
+        Move move = (Move) o;
+        return num == move.num;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(num);
+    }
 }

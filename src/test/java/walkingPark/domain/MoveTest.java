08154c5fafc7d4e611b1f4592714a5c9ff0aef47
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-02 16:36:23 +0900

    TEST : Move에 대한 단위 테스트

--- src/test/java/walkingPark/domain/MoveTest.java
+++ src/test/java/walkingPark/domain/MoveTest.java
@@ -1,2 +1,17 @@
-package walkingPark.domain;public class MoveTest {
+package walkingPark.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MoveTest {
+    @Test
+    void move_생성() {
+        assertThat(new Move(3)).isEqualTo(new Move(3));
+    }
+
+    @Test
+    void getNum() {
+        assertThat(new Move(3).getNum()).isEqualTo(3);
+    }
 }

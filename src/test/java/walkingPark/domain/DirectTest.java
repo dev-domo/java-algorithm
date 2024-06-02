148bcdd25068e3eccc3befd512454e0bb44b8db6
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-02 16:36:35 +0900

    TEST : Direct에 대한 단위 테스트

--- src/test/java/walkingPark/domain/DirectTest.java
+++ src/test/java/walkingPark/domain/DirectTest.java
@@ -1,2 +1,23 @@
-package walkingPark.domain;public class DirectTest {
+package walkingPark.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class DirectTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"E", "W", "S", "N"})
+    void direct_생성(String keyword) {
+        assertThat(new Direct(keyword)).isEqualTo(new Direct(keyword));
+    }
+
+    @Test
+    void 잘못된_direct_생성() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Direct("e"))
+                .withMessageMatching("잘못된 키워드입니다.");
+    }
 }
